bbd3c87a1aff406e5b52459f8795a8a6aa3b80b5
amrheshmat <amrheshmat95@gmail.com>
2024-03-30 14:59:00 +0200

    faq

--- Services/SampleMVC/Controllers/ContactUsController.cs
+++ Services/SampleMVC/Controllers/ContactUsController.cs
@@ -6,71 +6,71 @@ using TripBusiness.Ibusiness;
 
 namespace SampleMVC.Controllers
 {
-	public class ContactUsController : Controller
-	{
-		private IRepository _repo;
-		private ILocalizationService _localizationService;
-		private IMailService _mailService;
-		public ContactUsController(IRepositoryFactory repo, IMailService mailService, ILocalizationService localizationService)
-		{
-			_repo = repo.Create("AGGRDB");
-			_mailService = mailService;
-			_localizationService = localizationService;
-		}
-		public async Task<IActionResult> ContactUs()//index page
-		{
-			//List<Contact> contacts = await _repo.GetAll<Contact>().ToListAsync();
-			//ViewBag.contacts = contacts;
-			return View();
+    public class ContactUsController : Controller
+    {
+        private IRepository _repo;
+        private ILocalizationService _localizationService;
+        private IMailService _mailService;
+        public ContactUsController(IRepositoryFactory repo, IMailService mailService, ILocalizationService localizationService)
+        {
+            _repo = repo.Create("AGGRDB");
+            _mailService = mailService;
+            _localizationService = localizationService;
+        }
+        public async Task<IActionResult> ContactUs()//index page
+        {
+            //List<Contact> contacts = await _repo.GetAll<Contact>().ToListAsync();
+            //ViewBag.contacts = contacts;
+            return View();
 
-		}
-		[HttpPost]
-		public async Task<Response> Add([FromBody] Contact contactModel)
-		{
-			Response response = new Response();
-			response.Title = _localizationService.Localize("Contact");
-			response.Status = true;
-			var Contact = _repo.Create<Contact>(contactModel);
-			_repo.SaveChanges();
-			if (Contact != null)
-			{
-				MailRequest mailRequest = new MailRequest();
-				mailRequest.Contact = contactModel;
-				mailRequest.Subject = _localizationService.Localize("Contact");
-				mailRequest.ToEmail = new List<string>();
-				List<User> users = null;
-				decimal? permissionId = _repo.Filter<Permission>(permission => permission.permissionArea == "contact").FirstOrDefault().permissionId;
-				if (permissionId != null)
-				{
-					decimal? roleId = _repo.Filter<RolePermission>(e => e.permissionId == permissionId).FirstOrDefault().roleId;
-					if (roleId != null)
-					{
-						users = _repo.Filter<User>(e => e.roleId == roleId).ToList();
-					}
-				}
-				//get mails from user table base on permission ...
-				if (users != null)
-				{
-					foreach (var user in users)
-					{
-						mailRequest.ToEmail?.Add(user.email);
-					}
-				}
-				//send email to website users that have permission contacts ...
-				await _mailService.SendContactEmailAsync(mailRequest);
-				//send email to client ....
-				mailRequest.ToEmail = new List<string>();
-				mailRequest.ToEmail?.Add(contactModel.email);
-				mailRequest.Subject = _localizationService.Localize("ThankYou");
-				mailRequest.Contact.message = _localizationService.Localize("ThanksForContactWithUs") + ",<p>" + _localizationService.Localize("CheckSoon") + ".</p><p>" + _localizationService.Localize("Regards") + ",</p>";
-				await _mailService.SendContactThankEmailAsync(mailRequest);
-				response.Status = true;
-				response.Message = _localizationService.Localize("ContactSent");
-				return response;
-			}
-			response.Status = false;
-			response.Message = _localizationService.Localize("ContactSent");
-			return response;
-		}
-	}
+        }
+        [HttpPost]
+        public async Task<Response> Add([FromBody] Contact contactModel)
+        {
+            Response response = new Response();
+            response.Title = _localizationService.Localize("Contact");
+            response.Status = true;
+            var Contact = _repo.Create<Contact>(contactModel);
+            _repo.SaveChanges();
+            if (Contact != null)
+            {
+                MailRequest mailRequest = new MailRequest();
+                mailRequest.Contact = contactModel;
+                mailRequest.Subject = _localizationService.Localize("Contact");
+                mailRequest.ToEmail = new List<string>();
+                List<User> users = null;
+                decimal? permissionId = _repo.Filter<Permission>(permission => permission.permissionArea.ToLower() == "contact".ToLower()).FirstOrDefault().permissionId;
+                if (permissionId != null)
+                {
+                    decimal? roleId = _repo.Filter<RolePermission>(e => e.permissionId == permissionId).FirstOrDefault().roleId;
+                    if (roleId != null)
+                    {
+                        users = _repo.Filter<User>(e => e.roleId == roleId).ToList();
+                    }
+                }
+                //get mails from user table base on permission ...
+                if (users != null)
+                {
+                    foreach (var user in users)
+                    {
+                        mailRequest.ToEmail?.Add(user.email);
+                    }
+                }
+                //send email to website users that have permission contacts ...
+                await _mailService.SendContactEmailAsync(mailRequest);
+                //send email to client ....
+                mailRequest.ToEmail = new List<string>();
+                mailRequest.ToEmail?.Add(contactModel.email);
+                mailRequest.Subject = _localizationService.Localize("ThankYou");
+                mailRequest.Contact.message = _localizationService.Localize("ThanksForContactWithUs") + ",<p>" + _localizationService.Localize("CheckSoon") + ".</p><p>" + _localizationService.Localize("Regards") + ",</p>";
+                await _mailService.SendContactThankEmailAsync(mailRequest);
+                response.Status = true;
+                response.Message = _localizationService.Localize("ContactSent");
+                return response;
+            }
+            response.Status = false;
+            response.Message = _localizationService.Localize("ContactSent");
+            return response;
+        }
+    }
 }
--- Services/SampleMVC/Controllers/UserController.cs
+++ Services/SampleMVC/Controllers/UserController.cs
@@ -8,110 +8,110 @@ using TripBusiness.Ibusiness;
 
 namespace SampleMVC.Controllers
 {
-	public class UserController : Controller
-	{
-		private ILocalizationService _localizationService;
-		private IRepository _repo;
-		public UserController(ILocalizationService localizationService, IRepositoryFactory repo)
-		{
-			_repo = repo.Create("AGGRDB");
-			_localizationService = localizationService;
-		}
-		[AuthAttribute("user", "user")]
-		public async Task<IActionResult> User()//index page
-		{
-			List<User> users = await _repo.GetAll<User>().ToListAsync();
-			List<Role> roles = await _repo.GetAll<Role>().ToListAsync();
-			ViewBag.roles = roles;
-			ViewBag.users = users;
-			return View();
+    public class UserController : Controller
+    {
+        private ILocalizationService _localizationService;
+        private IRepository _repo;
+        public UserController(ILocalizationService localizationService, IRepositoryFactory repo)
+        {
+            _repo = repo.Create("AGGRDB");
+            _localizationService = localizationService;
+        }
+        [AuthAttribute("user", "user")]
+        public async Task<IActionResult> User()//index page
+        {
+            List<User> users = await _repo.GetAll<User>().ToListAsync();
+            List<Role> roles = await _repo.GetAll<Role>().ToListAsync();
+            ViewBag.roles = roles;
+            ViewBag.users = users;
+            return View();
 
-		}
-		[AuthAttribute("edit", "user")]
-		[HttpPost]
-		[Route("User/Edit")]
-		public async Task<Response> Edit([FromBody] User userModel)
-		{
-			Response response = new Response();
-			response.Title = _localizationService.Localize("Update");
-			var oldUSer = _repo.Filter<User>(e => e.userId == userModel.userId).FirstOrDefault();
-			if (oldUSer == null)
-			{
-				userModel.password = oldUSer != null ? oldUSer.password : null;
-				if (userModel.roleId == null)
-					userModel.roleId = oldUSer != null ? oldUSer.roleId : null;
-				if (oldUSer != null)
-				{
-					var user = _repo.context.Update(userModel);
-					await _repo.context.SaveChangesAsync();
-					response.Message = _localizationService.Localize("Updated");
-					response.Status = true;
-					return response;
-				}
-				response.Message = _localizationService.Localize("UpdatedError");
-				response.Status = false;
-				return response;
-			}
-			else
-			{
-				response.Message = _localizationService.Localize("UserNameExsits");
-				response.Status = false;
-				return response;
-			}
+        }
+        [AuthAttribute("edit", "user")]
+        [HttpPost]
+        [Route("User/Edit")]
+        public async Task<Response> Edit([FromBody] User userModel)
+        {
+            Response response = new Response();
+            response.Title = _localizationService.Localize("Update");
+            var oldUSer = _repo.Filter<User>(e => e.userId == userModel.userId).FirstOrDefault();
+            if (oldUSer != null)
+            {
+                userModel.password = oldUSer != null ? oldUSer.password : null;
+                if (userModel.roleId == null)
+                    userModel.roleId = oldUSer != null ? oldUSer.roleId : null;
+                if (oldUSer != null)
+                {
+                    var user = _repo.context.Update(userModel);
+                    await _repo.context.SaveChangesAsync();
+                    response.Message = _localizationService.Localize("Updated");
+                    response.Status = true;
+                    return response;
+                }
+                response.Message = _localizationService.Localize("UpdatedError");
+                response.Status = false;
+                return response;
+            }
+            else
+            {
+                response.Message = _localizationService.Localize("UserNameExsits");
+                response.Status = false;
+                return response;
+            }
 
-		}
-		[AuthAttribute("add", "user")]
-		[HttpPost]
-		[Route("User/Add")]
-		public async Task<Response> Add([FromBody] User userModel)
-		{
-			Response response = new Response();
-			response.Title = _localizationService.Localize("Add");
-			var oldUser = _repo.Filter<User>(e => e.userName == userModel.userName).FirstOrDefault();
-			if (oldUser == null)
-			{
-				userModel.creationDate = DateTime.Now;
-				userModel.password = MPSSecurity.Hash(userModel.password);
-				var user = await _repo.CreateAsync(userModel);
-				await _repo.context.SaveChangesAsync();
-				if (user?.userId != null)
-				{
-					response.Message = _localizationService.Localize("Added");
-					response.Status = true;
-					return response;
-				}
-				response.Message = _localizationService.Localize("AddedError");
-				response.Status = false;
-				return response;
-			}
-			else
-			{
-				response.Message = _localizationService.Localize("UserNameExsits");
-				response.Status = false;
-				return response;
-			}
+        }
+        [AuthAttribute("add", "user")]
+        [HttpPost]
+        [Route("User/Add")]
+        public async Task<Response> Add([FromBody] User userModel)
+        {
+            Response response = new Response();
+            response.Title = _localizationService.Localize("Add");
+            var oldUser = _repo.Filter<User>(e => e.userName == userModel.userName).FirstOrDefault();
+            if (oldUser == null)
+            {
+                userModel.creationDate = DateTime.Now;
+                userModel.password = MPSSecurity.Hash(userModel.password);
+                var user = await _repo.CreateAsync(userModel);
+                await _repo.context.SaveChangesAsync();
+                if (user?.userId != null)
+                {
+                    response.Message = _localizationService.Localize("Added");
+                    response.Status = true;
+                    return response;
+                }
+                response.Message = _localizationService.Localize("AddedError");
+                response.Status = false;
+                return response;
+            }
+            else
+            {
+                response.Message = _localizationService.Localize("UserNameExsits");
+                response.Status = false;
+                return response;
+            }
 
 
-		}
-		[AuthAttribute("delete", "user")]
-		[HttpGet]
-		[Route("User/Delete/{userId}")]
-		public async Task<Response> Delete(int userId)
-		{
-			Response response = new Response();
-			response.Title = _localizationService.Localize("Delete");
-			var user = _repo.Filter<User>(e => e.userId == userId).FirstOrDefault();
-			if (user != null)
-			{
-				_repo.Delete<User>(user);
-				await _repo.context.SaveChangesAsync();
-				response.Message = _localizationService.Localize("Deleted");
-				response.Status = true;
-				return response;
-			}
-			response.Message = _localizationService.Localize("DeletedError");
-			response.Status = false;
-			return response;
-		}
-	}
+        }
+        [AuthAttribute("delete", "user")]
+        [HttpGet]
+        [Route("User/Delete/{userId}")]
+        public async Task<Response> Delete(int userId)
+        {
+            Response response = new Response();
+            response.Title = _localizationService.Localize("Delete");
+            var user = _repo.Filter<User>(e => e.userId == userId).FirstOrDefault();
+            if (user != null)
+            {
+                _repo.Delete<User>(user);
+                await _repo.context.SaveChangesAsync();
+                response.Message = _localizationService.Localize("Deleted");
+                response.Status = true;
+                return response;
+            }
+            response.Message = _localizationService.Localize("DeletedError");
+            response.Status = false;
+            return response;
+        }
+    }
 }

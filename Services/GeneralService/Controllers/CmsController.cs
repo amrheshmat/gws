3674d21992e8a856cae4d69d42ad34d284b18d18
amrheshmat95 <amrheshmat95@gmail.com>
2025-04-14 22:19:39 +0200

    dashboard desgin

---
+++ Services/GeneralService/Controllers/CmsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MWS.Business.Shared;
+using MWS.Infrustructure.Repositories;
+using TripBusiness.Ibusiness;
+
+namespace SampleMVC.Controllers
+{
+    public class CmsController : Controller
+    {
+        private ILocalizationService _localizationService;
+        private readonly ILanguageService _languageService;
+        private IRepository _repo;
+        public CmsController(IRepositoryFactory repo, ILocalizationService localizationService, ILanguageService languageService)
+        {
+            _repo = repo.Create("AGGRDB");
+            _localizationService = localizationService;
+            _languageService = languageService;
+        }
+        [AuthAttribute("cms", "admin")]
+        [Route("admin/cms")]
+        public async Task<IActionResult> index()//index page
+        {
+            return View();
+        }
+    }
+}

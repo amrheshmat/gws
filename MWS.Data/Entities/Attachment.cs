7d932571e49e489bd2ab75662d8aa4170b29e925
amrheshmat95 <amrheshmat95@gmail.com>
2025-05-15 18:24:23 +0300

    blogs and tours

---
+++ MWS.Data/Entities/Attachment.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MWS.Data.Entities
+{
+    public partial class Attachment
+    {
+        [Key, Column(Order = 1)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public decimal? attachmentId { get; set; }
+
+        public string? attachmentPath { get; set; }
+        public string? attachmentName { get; set; }
+        public string? isMainAttachment { get; set; }
+        public string? type { get; set; }
+        public decimal? elementId { get; set; }
+    }
+}
